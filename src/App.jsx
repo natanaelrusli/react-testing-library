c9423ab3050f5dc477af26588178997cc51c9105
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-10-01 21:07:49 +0700

    add: test to check if input is empty

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container my-5">
+      
+    </div>
+  );
+}
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('inputs should be initially empty', () => {
+  render(<App />)
+  
+  // RTL queries
+  // https://testing-library.com/docs/queries/about
+  const emailInputElement = screen.getByRole('textbox')
+  
+  expect(emailInputElement.value).toBe('')
+})
