f2734578f68b13dcde5707a8cad77b8fa1b4fed2
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-10-01 21:08:08 +0700

    add: input to fulfill the test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="container my-5">
-      
+      <form action="">
+        <div className='mb-3'>
+          <label htmlFor="email" className='form-label'>
+            Email address
+          </label>
+          <input type="email" id='email' name='email' className='form-control' />
+        </div>
+      </form>
     </div>
   );
 }
