d4674d16c8b10b78a4cce8244f0403f52af63768
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-10-04 00:06:02 +0700

    add: snapshot testing

--- src/pages/Login/Login.test.js
+++ src/pages/Login/Login.test.js
@@ -1,9 +1,17 @@
 import Login from './Index'
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer'
 
 test('inputs should be initially empty', () => {
   render(<Login />)
 
   expect(screen.getByTestId('emailInput').value).toBe('')
   expect(screen.getByTestId('passwordInput').value).toBe('')
+})
+
+describe('snapshot testing', () => {
+  test('snapshot for login component', () => {
+    const login = renderer.create(<Login />).toJSON();
+    expect(login).toMatchSnapshot()
+  })
 })
